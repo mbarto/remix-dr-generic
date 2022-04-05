adc0092806a4b7ef4bafba22e6cd41ce4975dd78
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2022-04-05 15:55:04 +0200

    added loaders to routes

--- app/routes/here.tsx
+++ app/routes/here.tsx
@@ -1,12 +1,37 @@
-import { Outlet } from "@remix-run/react";
+import { json, LoaderFunction } from "@remix-run/node";
+import { Outlet, useLoaderData } from "@remix-run/react";
+
+export type DataItem = {
+    id: number
+    name: string
+}
+
+export type Data = {
+    items: DataItem[]
+}
+
+export const loader: LoaderFunction = ({context, request, params}) => {
+    return json<Data>({
+        items: [{
+            id: 1,
+            name: "first item"
+        }, {
+            id: 2,
+            name: "second item"
+        }]
+    })
+}
 
 export default function Here() {
-  return (
-    <div>
-      <h1>You are here!</h1>
-      <div>
-        <Outlet/>
-      </div>
-    </div>
-  );
+    const data = useLoaderData<Data>()
+    
+    return (
+        <div>
+        <h1>You are here!</h1>
+        <ul>{data?.items.map(i => <li key={i.id}>{i.name}</li>)}</ul>
+        <div>
+            <Outlet/>
+        </div>
+        </div>
+    );
 }
--- app/routes/here/nested.tsx
+++ app/routes/here/nested.tsx
@@ -1,7 +1,25 @@
+import { json, LoaderFunction } from "@remix-run/node";
+import { useLoaderData } from "@remix-run/react";
+import { Data } from "../here";
+
+export const loader: LoaderFunction = ({context, request, params}) => {
+    return json<Data>({
+        items: [{
+            id: 1,
+            name: "third item"
+        }, {
+            id: 2,
+            name: "fourth item"
+        }]
+    })
+}
+
 export default function Nested() {
+    const data = useLoaderData<Data>()
     return (
       <div>
         <div>This is the here subpage!</div>
+        <ul>{data?.items.map(i => <li key={i.id}>{i.name}</li>)}</ul>
       </div>
     );
   }
