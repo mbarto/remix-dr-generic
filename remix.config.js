759dc85ea8a9ed534ee0fac894063f71dfd0d7c8
Mauro Bartolomeoli <maurobartolomeoli@gmail.com>
2022-04-05 17:37:22 +0200

    added more files for vercel deploy

--- remix.config.js
+++ remix.config.js
@@ -3,7 +3,8 @@
  */
 module.exports = {
   ignoredRouteFiles: [".*"],
-  serverBuildTarget: "vercel"
+  serverBuildTarget: "vercel",
+  server: process.env.NODE_ENV === "development" ? undefined : "./server.js",
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // serverBuildPath: "build/index.js",
